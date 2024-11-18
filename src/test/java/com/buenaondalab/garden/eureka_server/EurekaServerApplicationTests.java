b1076d0695362c6d77c0f6e020e9a1f0ff9eb0de
andrea <pierdominici.andrea@gmail.com>
2024-11-18 01:40:57 +0100

    add end to end tests

--- src/test/java/com/buenaondalab/garden/eureka_server/EurekaServerApplicationTests.java
+++ src/test/java/com/buenaondalab/garden/eureka_server/EurekaServerApplicationTests.java
@@ -1,13 +1,37 @@
 package com.buenaondalab.garden.eureka_server;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.util.Map;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
+
+@SpringBootTest(webEnvironment = DEFINED_PORT)
 class EurekaServerApplicationTests {
 
+	@Autowired
+	private TestRestTemplate testRestTemplate;
+
 	@Test
-	void contextLoads() {
+	void catalogLoads() {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = testRestTemplate
+				.getForEntity("/eureka/apps", Map.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
 	}
+
+	@Test
+	void adminLoads() {
+		@SuppressWarnings("rawtypes")
+		ResponseEntity<Map> entity = testRestTemplate.getForEntity("/actuator/env", Map.class);
+		assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+	}
+
 
 }
